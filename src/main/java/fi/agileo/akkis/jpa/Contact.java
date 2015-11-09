aab219cd9109569af11c2a1afae002d0d6c739e5
Jari-Matti <j-m.s@netti.fi>
2015-11-09 10:43:36 +0200

    Repositoryn rakennemuutos

---
+++ src/main/java/fi/agileo/akkis/jpa/Contact.java
@@ -0,0 +1,14 @@
+package fi.agileo.akkis.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+public class Contact {
+
+}
