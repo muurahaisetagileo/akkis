a40d1af6934a55cfd6ebd63a704db388146a41c5
Jari-Matti <j-m.s@netti.fi>
2015-11-11 15:13:06 +0200

    Added deal.java new version and DealService.java base

--- src/main/java/fi/agileo/akkis/jpa/Deal.java
+++ src/main/java/fi/agileo/akkis/jpa/Deal.java
@@ -1,5 +1,8 @@
 package fi.agileo.akkis.jpa;
 
+
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,5 +26,67 @@ public class Deal {
 	
 	@Column(name="PERIODLENGTH")
 	private int periodLength;
+	
+	private Contract contract;
+	
+	private User user;
+	
+	private List<Contact> contacts;
+
+	public long getDealId() {
+		return dealId;
+	}
+
+	public void setDealId(long dealId) {
+		this.dealId = dealId;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getPeriodType() {
+		return periodType;
+	}
+
+	public void setPeriodType(int periodType) {
+		this.periodType = periodType;
+	}
+
+	public int getPeriodLength() {
+		return periodLength;
+	}
+
+	public void setPeriodLength(int periodLength) {
+		this.periodLength = periodLength;
+	}
+
+	public Contract getContract() {
+		return contract;
+	}
+
+	public void setContract(Contract contract) {
+		this.contract = contract;
+	}
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+
+	@Override
+	public String toString() {
+		return "Deal [dealId=" + dealId + ", price=" + price + ", periodType=" + periodType + ", periodLength="
+				+ periodLength + ", contract=" + contract + ", user=" + user + ", contacts=" + contacts + "]";
+	}
+	
+	
 	
 }
---
+++ src/main/java/fi/agileo/spring/service/DealService.java
@@ -0,0 +1,5 @@
+package fi.agileo.spring.service;
+
+public class DealService {
+
+}
