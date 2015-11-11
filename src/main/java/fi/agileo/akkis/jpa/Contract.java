66e1ab34fbb9a8f4a549c424c9eb0eaf7094cd4f
Jari-Matti <j-m.s@netti.fi>
2015-11-11 10:26:31 +0200

    Contract.java latest version

--- src/main/java/fi/agileo/akkis/jpa/Contract.java
+++ src/main/java/fi/agileo/akkis/jpa/Contract.java
@@ -1,5 +1,7 @@
 package fi.agileo.akkis.jpa;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,17 +21,65 @@ public class Contract {
 @Column(name="CONTRACT_ID")
 private long contractId;
 
-public String getContact() {
-return null;	
+@Column(name="CONTACT")
+private String contact;
+
+@Column(name="SIGNDATE")
+private String signdate;
+
+@Column(name="ENDDATE")
+private String enddate;
+
+@Column(name="CONTRACTSTATUS")
+private String contractstatus;
+
+
+public String getContact() 
+{
+	return this.contact;	
 }
 
-//Solmimispäivämäärä
-//Sopimuksen päättymispäivämäärä
-//Sopimuksen tila, signed tai delivered
+public void setContact(String contact)
+{		
+	this.contact = contact;
+}
+
+//signDate (Sopimuksen solmimispäivämäärä)
+public String getsignDate() 
+{
+	return this.signdate;	
+}
+
+public void setsignDate(String signdate) {
+	this.signdate = signdate;
+}
+
+//endDate (Sopimuksen päättymispäivämäärä)
+public String getendDate() {
+	return this.enddate;	
+}	
+
+public void setendDate(String enddate) {
+	this.enddate = enddate;
+}
+
+//status, signed or delivered (Sopimuksen tila)
+public String getcontractStatus() {
+	return this.contractstatus;
+	
+}
+
+public void setcontractStatus(String contractstatus) {
+	this.contractstatus = contractstatus;
+}
+
+@Override
+public String toString() {
+	return "Contract [contractId=" + contractId + ", contact=" + contact + ", signdate=" + signdate + ", enddate="
+			+ enddate + ", contractstatus=" + contractstatus + "]";
+}
+
+
 //Mäppäytyy edelleen kauppaan
 
-
-public void setContact(String contact){}		
-
 }
-
