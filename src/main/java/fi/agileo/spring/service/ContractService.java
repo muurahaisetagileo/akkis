63466b14d4c611bccfe6f82f79a6824166ece0d5
Harri Mäenpää <harri.maenpaa@gmail.com>
2015-11-12 10:14:45 +0200

    contact company and contract service operations, at least part of them

--- src/main/java/fi/agileo/spring/service/ContractService.java
+++ src/main/java/fi/agileo/spring/service/ContractService.java
@@ -1,9 +1,17 @@
 package fi.agileo.spring.service;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import fi.agileo.akkis.jpa.Contact;
+import fi.agileo.akkis.jpa.ContactCompany;
+import fi.agileo.akkis.jpa.Contract;
+import fi.agileo.akkis.jpa.User;
 
 @Component
 public class ContractService {
@@ -17,5 +25,22 @@ public class ContractService {
 	public void setEm(EntityManager em) {
 		this.em = em;
 	}
+	
+	@Transactional
+	public void createContract(
+			Contract contract,
+			User contractMakerUser, 
+			ContactCompany contactCompany,
+			List<Contact> contractMakerContacts) {
+	}
+	
+	@Transactional
+	public void addContactsToContract(Contract contract,
+			List<Contact> contactsToBeAdded) {
+		
+	}
 
+	public ContractInformation getContractPropertiesForView(Contract contract) {
+		return null;
+	}
 }
