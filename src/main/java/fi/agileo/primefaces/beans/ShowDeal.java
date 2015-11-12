16100ce8b3f585f47cd2a0afec53bac8d2531ff1
Jari-Matti <j-m.s@netti.fi>
2015-11-12 12:02:55 +0200

    ShowDeal.java update

--- src/main/java/fi/agileo/primefaces/beans/ShowDeal.java
+++ src/main/java/fi/agileo/primefaces/beans/ShowDeal.java
@@ -1,18 +1,50 @@
 package fi.agileo.primefaces.beans;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
+import fi.agileo.akkis.jpa.Contract;
 import fi.agileo.akkis.jpa.Deal;
+import fi.agileo.akkis.jpa.User;
+import fi.agileo.spring.service.ContractService;
 import fi.agileo.spring.service.DealService;
 
 @ManagedBean
 @SessionScoped
 public class ShowDeal {
+	private DealService dealService;
 	
+	private Deal deal;
 	
+	private List<Deal> deals;
+	
+	public DealService getDealService() {
+		return dealService;
+	}
 
+	public void setDealService(DealService dealService) {
+		this.dealService = dealService;
+	}
+	
+	public String viewDeal() {
+		/* fetch deal information into DealInformation object 
+		   and set its properties to members of this managed bean and 
+		   return the view page name */
+		
+		return "";
+	}
+
+	public Deal getDeal() {
+		return deal;
+	}
+
+	public void setDeal(Deal deal) {
+		this.deal = deal;
+	}
+	
 }
