e557dcd7d1658af6e1d015f1e35a3843bc8925e9
Jari-Matti <j-m.s@netti.fi>
2015-11-12 14:06:54 +0200

    JUnit added

---
+++ src/test/java/fi/agileo/akkis/test/JokuTesti.java
@@ -0,0 +1,14 @@
+package fi.agileo.akkis.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JokuTesti {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
