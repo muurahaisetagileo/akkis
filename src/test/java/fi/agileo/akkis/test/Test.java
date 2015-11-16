9c7800d93b9fc8d89b247d302697c926c372aa7c
Harri Mäenpää <harri.maenpaa@gmail.com>
2015-11-16 09:43:42 +0200

    extra jat removed from classpath, example of ignoring unneeded or not so critical test

--- src/test/java/fi/agileo/akkis/test/Test.java
+++ src/test/java/fi/agileo/akkis/test/Test.java
@@ -2,11 +2,14 @@ package fi.agileo.akkis.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
+
 public class Test {
 
+	@Ignore
 	@org.junit.Test
 	public void test() {
-		assertEquals("Ossi", "Ossi");
+		assertEquals("Ossi", "");
 	}
 
 }
