0bc25d8155f0416d97e61eff648377935586a21b
Jari-Matti <j-m.s@netti.fi>
2015-11-16 09:37:26 +0200

    Test2.java

---
+++ src/test/java/fi/agileo/akkis/test/Test2.java
@@ -0,0 +1,14 @@
+package fi.agileo.akkis.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.*;
+
+public class Test2 {
+	@Ignore
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
