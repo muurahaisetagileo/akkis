1687f51571de6cd00be31cf6a6871fced8a5b08d
Jari-Matti <j-m.s@netti.fi>
2015-11-17 13:39:13 +0200

    DealConsole.java updated with printing contact

--- src/main/java/fi/agileo/spring/service/console/DealConsole.java
+++ src/main/java/fi/agileo/spring/service/console/DealConsole.java
@@ -1,5 +1,33 @@
 package fi.agileo.spring.service.console;
 
+import org.springframework.stereotype.Component;
+
+import fi.agileo.akkis.jpa.Contact;
+import fi.agileo.akkis.jpa.ContactCompany;
+import fi.agileo.akkis.jpa.Contract;
+import fi.agileo.akkis.jpa.Deal;
+import fi.agileo.akkis.jpa.User;
+
+import fi.agileo.spring.service.ContactService;
+import fi.agileo.primefaces.beans.user.LoginUser;
+import fi.agileo.primefaces.beans.user.RegisterUser;
+
+@Component
 public class DealConsole {
+
+	public static void main(String[] args) {
+		// Create test deal
+		
+		Deal d = new Deal();
+		
+		d.setPrice(30.0);
+		d.setPeriodType(1);
+		d.setPeriodLength(1);
+		
+		Contact c = new Contact();
+		
+		System.out.println("Contact print results: " + c); 
+	
+	}
 
 }
